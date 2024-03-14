c24150da6e4d86f252cb06cbda82c96873537b96
DESKTOP-MRU8EUO\804 <olrang.2@gmail.com>
2024-03-14 10:49:22 +0900

    flutter Scaffold

--- chap01_flutter_basic/lib/main.dart
+++ chap01_flutter_basic/lib/main.dart
@@ -9,6 +9,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'App 타이틀',
+        //App의
+        theme: ThemeData(
+          textTheme: TextTheme(
+            bodyText1: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('AppBar'),
+            backgroundColor: Colors.blue,
+          ),
+          body: Text('Body 입니다'),
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'school',
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.access_alarm),
+            onPressed: () => {
+              print('hello'),
+            },
+          ),
+        ));
   }
 }
